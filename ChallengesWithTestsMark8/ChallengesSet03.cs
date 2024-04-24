b0e1db29f2521beb197e5ec3e29f43460ef328df
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-04-24 00:08:55 -0400

    Completed weekly 3 challenges

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -53,6 +53,11 @@ namespace ChallengesWithTestsMark8
 
         public bool ArrayContainsAFalse(bool[] vals)
         {
+            if (vals == null)
+            {
+                return false;
+            }
+
             foreach (var item in vals)
             {
                 if (item == false)
@@ -65,42 +70,109 @@ namespace ChallengesWithTestsMark8
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            int sum = 0;
+
+            foreach (var number in numbers)
+            {
+                //check if number is odd then add it to the sum
+                if (number % 2 != 0)
+                {
+                    sum += number;
+                }
+            }
+
+            //check if sum is odd
+            // return sum % 2 != 0; shorthand of below
+            if (sum % 2 != 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            return password.Any(char.IsNumber)
+                && password.Any(char.IsUpper)
+                && password.Any(char.IsLower) ? true : false;
+            //create three bool variables for isLetter, isDigit, isUpper
+            //bool isLetter = false;
+            //bool isDigit = false;
+            //bool isUpper = false;
+
+            ////loop through the password and check each letter for isLetter, isDigit, isUpper
+            //foreach(var letter in password)
+            //{
+            //    if (char.IsLower(letter))
+            //    {
+            //        isLetter = true;
+            //    }
+
+            //    if (char.IsUpper(letter))
+            //    {
+            //        isUpper = true;
+            //    }
+
+            //    if (char.IsDigit(letter))
+            //    {
+            //        isDigit = true;
+            //    }
+            //}
+            //return isLetter && isUpper && isDigit;
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            //return val[0];
+            return val.FirstOrDefault();
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            //return val[val.Length- 1];
+            return val.Last();
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            return divisor == 0 ? 0 : dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            //so population is 100 numbers
+            //int[] myArray = new int[50];     //half will be odd
+            //int index = 0; //seperate value for index that will run 50 times and will not be out of bounds of our array
+            var odds = new List<int>();
+            //we inc this index if i is odd...so i will iterate 100 times but index will only inc up to 50 times for odd values
+            //we can loop through 100 times with our i index value 
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    odds.Add(i);
+                }
+            }
+            return odds.ToArray(); ;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
+
         }
     }
 }
