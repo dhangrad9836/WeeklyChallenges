0beb777d2eb51e3f41e78046153cfabd991b6268
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-04-29 23:06:19 -0400

    Completed weekly challenges 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace ChallengesWithTestsMark8
@@ -30,6 +29,8 @@ namespace ChallengesWithTestsMark8
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
+            //add the params into a string
+            //find the length of each string and use .Min() to find shortest length
             List<int> smallestString = new List<int>();
             smallestString.Add(str1.Length);
             smallestString.Add(str2.Length);
@@ -40,37 +41,118 @@ namespace ChallengesWithTestsMark8
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            //List<int> smallestNumber = new List<int> { number1, number2, number3, number4 };
+            //return smallestNumber.Min();
+            var smallest = number1;
+            var list = new List<int> { number1, number2, number3, number4 };
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] < smallest)
+                {
+                    //make list[i] the new smallest number
+                    smallest = list[i];
+                }
+            }
+            return smallest;
+
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            //first check out the Business class declaration rt click- check definition/declaration
+            //you see that it's using a Name property so all we need to do is just set biz.Name = "TrueCoders";
+            biz.Name = "TrueCoders";
         }
 
+        //two sides must be greater than the third size
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            //if (sideLength1 == 0 || sideLength2 == 0 || sideLength3 == 0)
+            //{
+            //    return false;
+            //}
+            if ((sideLength1 + sideLength2 > sideLength3) && (sideLength1 + sideLength3 > sideLength1) && (sideLength2 + sideLength3 > sideLength1))
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            double num = 0;
+            var testNum = double.TryParse(input, out num);
+            return testNum;
         }
 
+        //we are checking if there are more null values vs non-null ie: int, string, etc in the array
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            //we need to loop throught the array and check if we have more "null" in the array which then is true
+            var nullCount = 0;
+            var nonNullCount = 0;
+            foreach (var obj in objs)
+            {
+                if (obj == null)
+                {
+                    nullCount++;
+                }
+                else
+                {
+                    nonNullCount++;
+                }
+            }//end foreach
+
+            if (nullCount > nonNullCount)
+            {
+                return true;
+            }
+
+
+            return false;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            var totalAvgEvens = 0;
+            var count = 0;
+
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    totalAvgEvens += num;
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                return 0;
+            }
+
+            //we need to parse totalAvgEvens into a double as the avg values can be double
+            return (double)totalAvgEvens / count;
+
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            //use recursive call until base case is resolved to calculate the factorial
+            return number * Factorial(number - 1);
+
+
         }
     }
 }
